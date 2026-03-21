87a73d71fa1922707f9e72aae888cde620c8986c
Shivam <sg641818@gmail.com>
2026-03-21 23:05:44 +0530

    fix: remove err in api name

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,17 @@ const Page = () => {
         </h1>
 
         <button
-          onClick={() => router.push("/climb")}
+          onClick={() => router.push("/camb")}
           className="w-full py-3 border border-white text-white rounded-lg transition hover:bg-white hover:text-black"
         >
-          Text to speech using climb
+          Text to speech using camb
         </button>
 
             <button
           onClick={() => router.push("/elevn")}
           className="w-full py-3 border border-white text-white rounded-lg transition hover:bg-white hover:text-black"
         >
-          Text to speech using 11Labs
+          Text to speech using elevenlabs(not working due to overeuse of free trial key)
         </button>
       </div>
     </div>
